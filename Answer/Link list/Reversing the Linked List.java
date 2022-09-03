6112d68c3bbb16a277f6e79928fc0807756366e6
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2022-09-03 22:29:23 +0530

    Create Reversing the Linked List.java

---
+++ Answer/Link list/Reversing the Linked List.java
@@ -0,0 +1,69 @@
+/* package whatever; // don't place package name! */
+
+import java.util.LinkedList;
+import java.util.Scanner;
+class Node
+{
+    int data;
+    Node next;
+ 
+    Node(int data, Node next)
+    {
+        this.data = data;
+        this.next = next;
+    }
+}
+public class Main
+{
+    // Helper function to print a given linked list
+    public static void printList(Node head)
+    {
+        Node data = head;
+        while (data != null)
+        {
+            System.out.print(ptr.data + " ");
+            data = data.next;
+        }
+    }
+    public static Node ReverseLinkedList(Node head)
+    {
+        if(head == null || head.next == null)
+        return head;
+
+        Node result = ReverseLinkedList(head.next);
+        head.next.next = head;
+
+        head.next = null;
+        return result;
+    }
+  
+    public static void main(String[] args)
+    {
+    	LinkedList list = new LinkedList();
+    	
+    	Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int keys[] = new int[size];
+        for(int i = 0; i < size; i++)
+        {
+        	keys[i] = sc.nextInt();
+        }
+ 
+        Node head = null;
+        for (int i = size - 1; i >= 0; i--) 
+        {
+            head = new Node(keys[i], head);
+        }
+        head = ReverseLinkedList(head);
+        printList(head);
+    }
+}
+
+
+// Input:
+// 5
+// 1 2 3 4 5
+  
+// Output:
+// 5 4 3 2 1
+ 
