e072bf2b22f5cbc53820ad0cb5c2cec455f1afd6
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2022-11-26 13:11:52 +0530

    Sort 0 1 2.java

--- Answer/Sorting/Sort 0 1 2.java
+++ Answer/Sorting/Sort 0 1 2.java
@@ -42,3 +42,29 @@ class Main{
         }   
     }
 }
+
+
+//2nd solution//
+  public static void sort012(int arr[], int n){
+	  int low = 0;
+	  int mid = 0;
+	  int high = n-1;    
+	  while(mid <= high)
+	  {
+	    if(arr[mid] == 0)
+	   {
+   		  swap(arr[mid] , arr[low]);
+		  mid++;
+		  low++;
+	   }
+	   else if(arr[mid] == 1)
+	   {
+		  mid++;
+	   }
+	   else
+	   {
+		  swap(arr[mid] , arr[high]);
+		  high--;
+	   }
+	}
+ }
